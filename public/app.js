ee5040882c3d4fb1ea4286fd6f723b06f5f42c3d
DebMeade <dkmeade@gmail.com>
2018-10-08 20:47:30 -0600

    updating style

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br /" + data[i].summary + "</p");
   }
 });
 
@@ -17,7 +17,7 @@ $(document).on("click", "p", function() {
     $("#notes").append("<h2>" + data.title + "</h2>");
     $("#notes").append("<input id='titleinput' name='title' >");
     $("#notes").append("<textarea id = 'bodyinput' name='body'></textarea");
-    $("#notes").append("<button data-id='" + data._id + "'id='davenote'>Save Note</button");
+    $("#notes").append("<button data-id='" + data._id + "'id='savenote'>Save Note</button");
 
     if(data.note) {
       $("#titleinput").val(data.note.title);
