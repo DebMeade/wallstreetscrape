151058ad46902f06ea29aa8a8b6b234b65082021
DebMeade <dkmeade@gmail.com>
2018-10-10 15:17:09 -0600

    things

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/wallstreetscrape", { useNewUrlParser: true });
+mongoose.connect(process.env.MONDODB_URI || "mongodb://localhost/wallstreetscrape", { useNewUrlParser: true });
 
 //Routes
 
