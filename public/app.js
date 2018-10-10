454ebbd184f1b1c055985af35233959959c481b6
DebMeade <dkmeade@gmail.com>
2018-10-09 21:05:14 -0600

    corrected note saving functionality

--- public/app.js
+++ public/app.js
@@ -1,10 +1,18 @@
 $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br /" + data[i].summary + "</p");
+   
+    var articleHtml = `<div>
+                        <h3 data-id = "${data[i]._id}">${data[i].title}</h3>
+                        <p> ${data[i].summary} </p>
+                        <a href = "${data[i].link}">Link to Article</a>
+                        </div>`;
+    
+     $("#articles").append(articleHtml);
+    
   }
 });
 
-$(document).on("click", "p", function() {
+$(document).on("click", "h3", function() {
   $("#notes").empty();
   var thisId = $(this).attr("data-id");
 
@@ -16,8 +24,8 @@ $(document).on("click", "p", function() {
     console.log(data);
     $("#notes").append("<h2>" + data.title + "</h2>");
     $("#notes").append("<input id='titleinput' name='title' >");
-    $("#notes").append("<textarea id = 'bodyinput' name='body'></textarea");
-    $("#notes").append("<button data-id='" + data._id + "'id='savenote'>Save Note</button");
+    $("#notes").append("<textarea id = 'bodyinput' name='body'></textarea>");
+    $("#notes").append("<button data-id='" + data._id + "'id='savenote'>Save Note</button>");
 
     if(data.note) {
       $("#titleinput").val(data.note.title);
@@ -27,14 +35,14 @@ $(document).on("click", "p", function() {
 });
 
 $(document).on("click", "#savenote", function() {
-  var thisId = $(this).attr(data-id);
+  var thisId = $(this).attr("data-id");
 
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
       title: $("#titleinput").val(),
-      body: $("#bodyinput".val())
+      body: $("#bodyinput").val()
     }
   })
     .then(function(data) {
